1369060c462dc3ccb459b15aae637d14d64cf407
Ashish yaduveer <ashishyaduveer791@gmail.com>
2026-02-09 13:01:00 +0530

    Add files via upload

---
+++ questionlaB31.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int sum(int a[], int n)
+{
+    if (n == 0)
+        return 0;
+    return a[n - 1] + sum(a, n - 1);
+}
+
+int main()
+{
+    int n, i, a[50];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Sum of elements = %d", sum(a, n));
+
+    return 0;
+}
